d301bbb7727a2768c1228c049ae2dd6adc1dec89
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-05-24 20:22:36 -0300

    Add build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+TAG="$BRANCH"
+
+if [[ "$BRANCH" == "master" ]]; then
+	TAG = "latest"
+fi
+
+exec docker buildx build . -t luksamuk/psxtoolchain:$TAG --push
+
