9629ef790d0ac4b883c853a0aa9bc110b755606b
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-05-24 20:26:06 -0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ BRANCH=`git rev-parse --abbrev-ref HEAD`
 TAG="$BRANCH"
 
 if [[ "$BRANCH" == "master" ]]; then
-	TAG = "latest"
+	TAG=latest
 fi
 
 exec docker buildx build . -t luksamuk/psxtoolchain:$TAG --push
